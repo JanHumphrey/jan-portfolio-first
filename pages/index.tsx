96336fff619285b2f2e0b20d61ff0fd7065c462e
JanHumphrey <sjanhumphrey@gmail.com>
2023-02-04 22:23:57 +0800

    legacyBehavior fixed for Link tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,39 +5,37 @@ import Link from 'next/link';
 
 
 function Home() {
-  return (
-    <>
-      <Head>
-        <title>Jan Humphrey - Software Developer</title>
-      </Head>
-      {/* HERO SECTION */}
-      <section className=''>
-        <article className="">
-          <div>
-            <span>Jan</span>
-          </div>
-          <div>
-            <span>Humphrey</span>
-          </div>
-          <div>
-            <span>FULL-STACK DEVELOPER | UI/UX DESIGNER | FREELANCER</span>
-          </div>
-          <div>
-            <h1>A passionate designer and programmer who promotes efficiency in every interactive design for your web identity.</h1>
-          </div>
-          <div>
-            <Link href="/contact">
-            <a>Contact Me</a>
-            </Link>
-            <Link href="/contact">
-            <a>Book a Meeting</a>
-            </Link>
-          </div>
-        </article>
-      </section>
-      {/* END HERO SECTION */}
-    </>
-  );
+  return <>
+    <Head>
+      <title>Jan Humphrey - Software Developer</title>
+    </Head>
+    {/* HERO SECTION */}
+    <section className=''>
+      <article className="">
+        <div>
+          <span>Jan</span>
+        </div>
+        <div>
+          <span>Humphrey</span>
+        </div>
+        <div>
+          <span>FULL-STACK DEVELOPER | UI/UX DESIGNER | FREELANCER</span>
+        </div>
+        <div>
+          <h1>A passionate designer and programmer who promotes efficiency in every interactive design for your web identity.</h1>
+        </div>
+        <div>
+          <Link href="/contact">
+            Contact Me
+          </Link>
+          <Link href="/contact">
+            Book a Meeting
+          </Link>
+        </div>
+      </article>
+    </section>
+    {/* END HERO SECTION */}
+  </>;
 };
 
 export default Home
