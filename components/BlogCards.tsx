bec7297fbbcd573f50b13a15ff9a76f47141323f
JanHumphrey <sjanhumphrey@gmail.com>
2023-02-04 23:50:45 +0800

    Added BlogCards and ProjectCards Components

---
+++ components/BlogCards.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+interface BlogCardsProps {
+  imageURL: string;
+  title: string;
+  description: string;
+  url?: string;
+}
+
+export const BlogCards = (props: BlogCardsProps) => {
+  return 
+};
+export default BlogCards;
---
+++ components/ProjectCards.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+interface ProjectCardProps {
+  imageURL: string;
+  title: string;
+  description: string;
+  url?: string;
+}
+
+export const ProjectCards = (props: ProjectCardProps) => {
+  return 
+};
+export default ProjectCards;
