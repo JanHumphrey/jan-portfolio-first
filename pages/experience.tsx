2c5a664366178ce4bcd1ac1d4771abf3686de0a9
JanHumphrey <sjanhumphrey@gmail.com>
2023-02-05 08:42:13 +0800

    Added Experience page

---
+++ pages/experience.tsx
@@ -0,0 +1,21 @@
+
+function Experience() {
+  return (
+    <>
+      <section>
+      <h1>Projects</h1>
+        <article>
+          <h2>Building websites that are both functional and visually appealing is an art form. Let's dive into my collection of web development masterpieces.
+          </h2>
+        </article>
+         <div>
+            <div>
+              {/* PROJECTS COMPONENTS */}
+            </div>
+         </div>
+      </section>
+    </>
+  )
+}
+
+export default Experience
