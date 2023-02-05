d8a2e6b4e0e42c58e9cfa9899d3bdb6c84f13b7c
JanHumphrey <sjanhumphrey@gmail.com>
2023-02-05 09:34:51 +0800

    Contact page markup done

---
+++ pages/contact.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head"
+
+function Contact() {
+  return (
+    <>
+    <Head>
+      <title>Jan Humphrey - Contact</title>
+    </Head>
+      <section>
+        <div>
+          <h1>Contact</h1>
+        </div>
+        <div>
+          <div>
+            <header>
+              <h1>Get in touch, let's talk.</h1>
+              <p>Feel free to send me an <a href="mailto:sjanhumphrey@gmail.com">email</a>  or schedule a <a href="https://calendly.com/janhumphrey/consultation">free consultation</a>  with me</p>
+              <p>sjanhumphrey@gmail.com</p>
+            </header>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Contact
