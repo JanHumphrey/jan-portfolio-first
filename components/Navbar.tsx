508ae682dfc0ee17e547bc2ac27269aa366d04ae
JanHumphrey <sjanhumphrey@gmail.com>
2023-02-05 10:54:32 +0800

    Added links for navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,81 @@
 import Image from "next/image"
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+
+const NavlinkDesktop = ({
+  name,
+  url,
+  isActive,
+}: {
+  name: string;
+  url: string;
+  isActive: boolean;
+}) => {
+  return (
+    <li
+      className={`py-12 transition-all px-7 ${isActive ? "font-bold" : "font-medium"
+        }`}
+    >
+      <Link href={url}>
+        <a>{name}</a>
+      </Link>
+    </li>
+  );
+};
+
 
 function Navbar() {
+  const [currentLink, setCurrentLink] = useState("");
+  const router = useRouter();
+  useEffect(() => {
+    setCurrentLink(router.pathname);
+    console.log(router.pathname);
+  }, [router.pathname]);
+
   return (
     <>
       <nav>
+        {/* LOGO */}
         <div>
-          {/* LOGO */}
           <Image
             width={57}
             height={57}
             src={"/assets/images/logo.svg"}
             alt="Jan Humphrey Logo"
           />
+        </div>
+
+        {/* NAVIGATIONS */}
+        <div>
+          <ul>
+            <NavlinkDesktop
+              name="Home"
+              url="/"
+              isActive={currentLink === "/"}
+            />
+            <NavlinkDesktop
+              name="About"
+              url="/about"
+              isActive={currentLink === "/about"}
+            />
+            <NavlinkDesktop
+              name="Projects"
+              url="/projects"
+              isActive={currentLink === "/projects"}
+            />
+            <NavlinkDesktop
+              name="Experience"
+              url="/experience"
+              isActive={currentLink === "/experience"}
+            />
+            <NavlinkDesktop
+              name="Contact"
+              url="/contact"
+              isActive={currentLink === "/contact"}
+            />
+          </ul>
         </div>
       </nav>
     </>
