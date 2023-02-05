710cf42c3eb87358b425c5d985a5bbb883f51252
JanHumphrey <sjanhumphrey@gmail.com>
2023-02-05 18:01:17 +0800

    Added footer markup done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,76 @@
-function Footer(){
-  return
+import Link from "next/link";
+import {
+  AiFillLinkedin,
+  AiFillTwitterSquare,
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillGithub,
+} from "react-icons/ai";
+
+const SocialLink = ({ icon, url }: { icon: React.ReactNode; url: string }) => {
+  return (
+    <Link href={url}>
+      <a className="px-2 pb-2" target='_blank'>{icon}</a>
+    </Link>
+  );
+};
+
+
+function Footer() {
+  return (
+    <>
+      <div>
+        {/* LINE HERE */}
+        <div></div>
+        <div>
+          <div>
+            <p>Jan Humphrey</p>
+          </div>
+          <div>
+            <ul>
+              <SocialLink
+                icon={
+                  <AiFillInstagram
+                    size={35}
+
+                  />
+                }
+                url="https://www.instagram.com/jnhmphry/"
+              />
+              <SocialLink
+                icon={
+                  <AiFillGithub
+                    size={35}
+
+                  />
+                }
+                url="https://github.com/JanHumphrey"
+              />
+              <SocialLink
+                icon={
+                  <AiFillFacebook
+                    size={35}
+
+                  />
+                }
+                url="https://www.facebook.com/JnHmphry/"
+              />
+              <SocialLink
+                icon={
+                  <AiFillLinkedin
+                    size={35}
+
+                  />
+                }
+                url="https://www.linkedin.com/in/janhumphrey/"
+              />
+
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Footer
