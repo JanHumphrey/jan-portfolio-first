9e245513981c30a08746f68923a7cb4227fca2f7
JanHumphrey <sjanhumphrey@gmail.com>
2023-02-19 20:46:14 +0800

    Initial fix desktop responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,31 +18,48 @@ function Home() {
     {/* HERO SECTION */}
     <section className='flex flex-col'>
 
-      <article className="flex-col order-3 px-7">
-        <div className='relative flex flex-col '>
-          <span className="absolute font-hero text-9xl uppercase ">Jan</span>
-          <span className=" font-hero2 text-7xl uppercase mt-28">Humphrey</span>
-        </div>
+      <article className="flex-col order-3 px-7 lg:px-24">
+        <div className='flex flex-col lg:flex-row lg:justify-center lg:space-x-1 lg:gap-40'>
+          <div className='lg:w-1/2 '>
 
-        <div>
-          <span className='font-main font-bold text-xs leading-tight'>FULL-STACK DEVELOPER | UI/UX DESIGNER | FREELANCER</span>
-        </div>
 
-        <div>
-          <h1 className='font-main font-bold text-sm py-6'>A passionate designer and programmer who promotes efficiency in every interactive design for your web identity.</h1>
-        </div>
+            <div className='relative flex flex-col '>
+              <span className="absolute font-hero text-9xl uppercase lg:text-[225px]">Jan</span>
+              <span className=" font-hero2 text-7xl uppercase mt-28 lg:mt-56 lg:text-[125px]">Humphrey</span>
+            </div>
 
-        <div className='font-main2 flex flex-row space-x-5 justify-center '>
-          <Link href="/contact" className='flex flex-row items-center  justify-center bg-slate-800 hover:bg-black text-white py-2 px-4 rounded-lg md:py-3 md:px-6 h-11 w-48 drop-shadow-md'>
-            <CiMail /><span className='ml-2'>Contact Me</span>
-          </Link>
-          <Link href="/contact" className='flex flex-row items-center justify-center  bg-slate-200 hover:bg-slate-400 text-black py-2 px-4 rounded-lg md:py-3 md:px-6 h-11 w-56 drop-shadow-md'>
-            <BiCalendar /><span className='ml-2'>Book a Meeting</span>
-          </Link>
-        </div>
-      </article>  
+            <div>
+              <span className='font-main font-bold text-xs leading-tight lg:text-lg lg:mt-3'>FULL-STACK DEVELOPER | UI/UX DESIGNER | FREELANCER</span>
+            </div>
 
-      <div className='flex items-center mt-2'>
+            <div className='lg:w-auto'>
+              <h1 className='font-main font-bold text-sm py-6 lg:text-xl lg:text-wrap lg:w-[500px]'>A passionate designer and programmer who promotes efficiency in every interactive design for your web identity.</h1>
+            </div>
+
+            <div className='font-main2 flex flex-row space-x-5 justify-center lg:w-[500px] lg:justify-start '>
+              <Link href="/contact" className='transition flex flex-row items-center  justify-center bg-black hover:bg-slate-800 text-white py-2 px-4 rounded-lg md:py-3 md:px-6 h-11 w-48 drop-shadow-md'>
+                <CiMail /><span className='ml-2'>Contact Me</span>
+              </Link>
+              <Link href="/contact" className='transition flex flex-row items-center justify-center  bg-slate-200 hover:bg-slate-300 text-black py-2 px-4 rounded-lg md:py-3 md:px-6 h-11 w-56 drop-shadow-md'>
+                <BiCalendar /><span className='ml-2'>Book a Meeting</span>
+              </Link>
+            </div>
+          </div>
+
+          <div className='hidden md:block lg:block lg:items-start '>
+            <Image
+              width={300}
+              height={550}
+              src={"/assets/images/hero-image.png"}
+              alt="Jan Humphrey Salangsang"
+              className='w-48 mb-3 lg:w-auto '
+            />
+          </div>
+
+        </div>
+      </article>
+
+      <div className='flex items-center mt-2 lg:hidden'>
         <div className='flex order-1 w-1/2 text-xl justify-center space-x-5 '>
           <NavbarMobile />
         </div>
@@ -62,10 +79,11 @@ function Home() {
 
     {/* RECENT PROJECTS */}
     <section>
-      <div className='flex flex-row mt-28 px-7 justify-between font-main2 items-center'>
-        <h2 className='text-5xl font-bold '>Recent Projects</h2>
-        <Link href="/projects" className='flex flex-row items-center  justify-center bg-slate-800 hover:bg-black text-white py-2 px-4 rounded-lg md:py-3 md:px-6 h-11 w-48 drop-shadow-md'>
-        <BsBoxArrowUpRight/>  <span className='ml-2'>View All</span>
+      <div className='flex flex-row mt-28 px-7 justify-between font-main2 items-center lg:mx-60'>
+        <h2 className='text-4xl font-bold lg:ml-2'>Recent Projects <span className='inline-block w-4 h-4 bg-violet-500 rounded-full'></span></h2>
+        <Link href="/projects" className='flex flex-row items-center  justify-center bg-slate-800 hover:bg-black
+      text-white py-2 px-4 rounded-lg md:py-3 md:px-6 h-11 w-48 drop-shadow-md'>
+          <BsBoxArrowUpRight />  <span className='transition ml-2'>View All</span>
         </Link>
       </div>
 
@@ -93,10 +111,11 @@ function Home() {
     </section>
     <section>
 
-      <div className='flex flex-row mt-28 px-7 justify-between font-main2'>
-        <h2 className='text-5xl font-bold pr-24'>Blog Post</h2>
-        <Link href="/blogs" className='flex flex-row items-center  justify-center bg-slate-800 hover:bg-black text-white py-2 px-4 rounded-lg md:py-3 md:px-6 h-11 w-48 drop-shadow-md'>
-       <BsBoxArrowUpRight/> <span className='ml-2'>View All</span>
+      <div className='flex flex-row mt-28 px-7 justify-between font-main2 items-center lg:mx-60'>
+        <h2 className='text-4xl font-bold pr-24 lg:ml-2'>Blog Post <span className='inline-block w-4 h-4 bg-violet-500 rounded-full'></span></h2>
+        <Link href="/blogs" className='flex flex-row items-center  justify-center bg-slate-800 hover:bg-black text-white py-2 px-4 
+        rounded-lg md:py-3 md:px-6 h-11 w-48 drop-shadow-md'>
+          <BsBoxArrowUpRight /> <span className='transition ml-2'>View All</span>
         </Link>
       </div>
 
