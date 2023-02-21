061ac28fd2b44514b91ea85421d05928454c6294
JanHumphrey <sjanhumphrey@gmail.com>
2023-02-21 23:27:42 +0800

    Add padding bottom

--- components/Layouts/BlankLayout.tsx
+++ components/Layouts/BlankLayout.tsx
@@ -4,7 +4,7 @@ import Navbar from "../Navbar/GeneralNavbar"
 
 export const BlankLayout= ({children}: {children: React.ReactNode}) => {
   return (
-    <div className="flex flex-col max-w-screen-2xl mx-auto">
+    <div className="flex flex-col max-w-screen-2xl mx-auto pb-52 lg:pb-0">
       <Navbar />
       {children}
       <Footer />
