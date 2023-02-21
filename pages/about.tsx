e6ab952060f1a7de8c2fe570e1ff576a46a64870
JanHumphrey <sjanhumphrey@gmail.com>
2023-02-21 23:47:21 +0800

    Align each div for desktop

--- pages/about.tsx
+++ pages/about.tsx
@@ -64,42 +64,40 @@ function About() {
 
           {/* INTRODUCTORY  */}
 
-          <div className="flex flex-col lg:flex-row">
-            <div className="order-1 font-main font-bold text-xs mt-32 lg:order-2 lg:mt-0">
+          <div className="flex flex-col lg:flex-row lg:mt-[212px] lg:gap-36">
+            <div className="order-1 font-main font-bold text-xs mt-32 lg:order-2 lg:mt-[80px]">
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis minus adipisci quaerat excepturi cupiditate dolor dignissimos saepe provident, labore iusto beatae, porro sint aspernatur accusamus. Voluptas recusandae harum quam ratione.</p><br />
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora corrupti cupiditate adipisci vero quas laborum vel, ratione neque possimus id quaerat repellendus facere eius error aspernatur, asperiores recusandae magni placeat?</p><br />
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est odit rem placeat veritatis neque ipsam repellendus, aut eius. Quibusdam consequuntur natus ipsam sit illo obcaecati officiis quisquam fugiat. Iure, dicta.</p>
 
               <div className="hidden md:block order-2 mt-6 lg:order-3">
-              <h1 className='font-main2 text-4xl font-bold 2xl:ml-10 lg:text-6xl'>Technology Stack <span className='inline-block w-3 h-3 bg-violet-500 rounded-full'></span></h1>
-              {/* STACK COMPONENTS HERE */}
-              <div>
+                <h1 className='font-main2 text-4xl font-bold 2xl:ml-10 lg:text-5xl'>Technology Stack <span className='inline-block w-3 h-3 bg-violet-500 rounded-full'></span></h1>
+                {/* STACK COMPONENTS HERE */}
+                <div>
 
+                </div>
+              </div>
+              <div className="hidden md:block order-3 lg:order-4">
+                <h1 className='font-main2 text-4xl font-bold 2xl:ml-10 lg:text-5xl'>Recommendations <span className='inline-block w-3 h-3 bg-violet-500 rounded-full'></span></h1>
+                {/* RECOMMENDATIONS COMPONENTS HERE */}
+                <div>
+                </div>
               </div>
             </div>
-            <div className="hidden md:block order-3 lg:order-4">
-              <h1 className='font-main2 text-4xl font-bold 2xl:ml-10 lg:text-6xl'>Recommendations <span className='inline-block w-3 h-3 bg-violet-500 rounded-full'></span></h1>
-              {/* RECOMMENDATIONS COMPONENTS HERE */}
-              <div>
-              </div>
-            </div>
-            </div>
-           
-            
 
             <div className="order-4 flex flex-col mt-6 space-y-8 lg:order-1">
               {/* CTA'S */}
               <div>
 
-                <h1 className='font-main2 text-4xl font-bold 2xl:ml-10 lg:text-6xl mb-2'>Contact <span className='inline-block w-3 h-3 bg-violet-500 rounded-full'></span></h1>
+                <h1 className='font-main2 text-4xl font-bold 2xl:ml-10 lg:text-5xl mb-2'>Contact <span className='inline-block w-3 h-3 bg-violet-500 rounded-full'></span></h1>
                 <p className="font-main2">For assistance or questions, please don't hesitate to reach out to me via <a href="mailto:sjanhumphrey@gmail.com">email</a> and I'll respond promptly.</p>
               </div>
               <div>
-                <h1 className='font-main2 text-4xl font-bold 2xl:ml-10 lg:text-6xl mb-2'>Hire Me <span className='inline-block w-3 h-3 bg-violet-500 rounded-full'></span></h1>
+                <h1 className='font-main2 text-4xl font-bold 2xl:ml-10 lg:text-5xl mb-2'>Hire Me <span className='inline-block w-3 h-3 bg-violet-500 rounded-full'></span></h1>
                 <p className="font-main2">Book a zero-cost <a>consultation</a> with me and let's have a conversation about your project.</p>
               </div>
               <div>
-                <h1 className='font-main2 text-4xl font-bold 2xl:ml-10 lg:text-6xl mb-2 '>Social Links <span className='inline-block w-3 h-3 bg-violet-500 rounded-full'></span></h1>
+                <h1 className='font-main2 text-4xl font-bold 2xl:ml-10 lg:text-5xl mb-2'>Social Links <span className='inline-block w-3 h-3 bg-violet-500 rounded-full'></span></h1>
                 <div className="font-main2 space-y-3">
                   <div>
                     <a href="https://www.facebook.com/JnHmphry/">
@@ -130,23 +128,23 @@ function About() {
               </div>
 
             </div>
-            </div>
-            
-            <div className="block md:hidden order-2 mt-6 lg:order-3">
-              <h1 className='font-main2 text-4xl font-bold 2xl:ml-10 lg:text-6xl'>Technology Stack <span className='inline-block w-3 h-3 bg-violet-500 rounded-full'></span></h1>
-              {/* STACK COMPONENTS HERE */}
-              <div>
+          </div>
 
-              </div>
+          <div className="block md:hidden order-2 mt-6">
+            <h1 className='font-main2 text-4xl font-bold 2xl:ml-10'>Technology Stack <span className='inline-block w-3 h-3 bg-violet-500 rounded-full'></span></h1>
+            {/* STACK COMPONENTS HERE */}
+            <div>
+
             </div>
-            <div className="block md:hidden order-3 lg:order-4">
-              <h1 className='font-main2 text-4xl font-bold 2xl:ml-10 lg:text-6xl'>Recommendations <span className='inline-block w-3 h-3 bg-violet-500 rounded-full'></span></h1>
-              {/* RECOMMENDATIONS COMPONENTS HERE */}
-              <div>
-              </div>
+          </div>
+          <div className="block md:hidden order-3">
+            <h1 className='font-main2 text-4xl font-bold 2xl:ml-10'>Recommendations <span className='inline-block w-3 h-3 bg-violet-500 rounded-full'></span></h1>
+            {/* RECOMMENDATIONS COMPONENTS HERE */}
+            <div>
             </div>
-            
-          
+          </div>
+
+
         </div>
       </section>
     </>
